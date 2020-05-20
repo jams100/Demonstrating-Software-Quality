b960ff037ec797a482fb2c977b3b9395e551ee27
jams100 <jamesredmond25@gmail.com>
2020-05-20 03:05:11 +0100

    Testing to see if Survey question ArrayList exists

--- project/src/test/java/project/AppTest.java
+++ project/src/test/java/project/AppTest.java
@@ -2,17 +2,30 @@ package project;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
- 
+
     @Test
     public void createSurveyEmpty() {
         Survey s = new Survey();
         assertTrue("Should be an object of type survey", s instanceof Survey);
     }
 
+    //Test to create a survey with a name and questions
+    @Test
+    public void createSurvey() {
+        Question one = new Question("Food Liked");
+        // ArrayList to store questions
+        ArrayList<Question> questions = new ArrayList<Question>();
+        questions.add(one);
+
+        Survey s = new Survey("My Questions", questions);
+        assertTrue("Should be object of type survey", s instanceof Survey);
+    }
 }
